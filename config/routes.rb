baf709d5831674b04b699b2f671ac95fedea1a92
sangeeta.pillai <sangeeta.pillai@nielsen.com>
2018-10-27 11:59:55 +0530

    Final changes for welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/index'
-  get 'welcome/login'
-  get 'welcome/forgotpassword'
-  get 'welcome/register'
+  #get 'welcome/login'
+  #get 'welcome/forgotpassword'
+  #get 'welcome/register'
+  #get 'welcome/loggedinindex'
 
-  get 'app/dashboard'
-  get 'app/home'
-  get 'app/blog'
+  #get 'app/dashboard'
+  #get 'app/home'
+  #get 'app/blog'
 end
