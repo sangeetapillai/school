d18482889182703e7a39361fb640123104459e54
sangeeta.pillai <sangeeta.pillai@nielsen.com>
2018-10-28 10:34:35 +0530

    moving everything to files

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,16 @@
 class WelcomeController < ApplicationController
     def index     	
-    	
+    	contact_details = {}
+        file_path = File.join Rails.root, 'public',"contact_details.txt"
+        File.open(file_path) do |fp|
+          fp.each do |line|
+            key, value = line.chomp.split('*')
+            contact_details[key] = value
+          end
+        end
+        @phone = contact_details['phone'] 
+        @email = contact_details['email'] 
+        @facebook = contact_details['facebook'] 
     end
 
      def loggedinindex     	
